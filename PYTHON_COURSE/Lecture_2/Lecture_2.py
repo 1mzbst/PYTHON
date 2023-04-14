3936d8014fae3361c5a93b2a2d6c11bb9e9550df
Dmitriy Kulakov <dk4git@outlook.com>
2023-04-14 21:54:04 +0300

    almost path

---
+++ PYTHON_COURSE/Lecture_2/Lecture_2.py
@@ -0,0 +1,10 @@
+list_1 = []
+print(list_1)
+for i in range (5):
+    list_1.append(i)
+    print(list_1)
+# list_1 = [1,3,5,77,2]
+print(list_1.pop(3))
+print(list_1)
+list_1.insert(3, 'yippe_ki_yey')
+print(list_1) 
