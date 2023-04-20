9e72d8c17297393319ca10147765fd9bc2544c52
Dmitriy Kulakov <dk4git@outlook.com>
2023-04-20 20:25:30 +0300

    Add Tasks 16 18 20, and 16 is done

---
+++ PYTHON_COURSE/Home_Work/Task_16.py
@@ -0,0 +1,27 @@
+# Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N].
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+
+# *Пример:*
+
+# 5
+#     1 2 3 4 5
+#     3
+#     -> 1
+
+#Решение
+array = []
+count = 0
+numN = int(input('Enter the length of array(N): '))
+print('Enter the digits of the list: ')
+for i in range(1,numN+1):
+    i = int(input())
+    array.append(i)
+print(array)
+find_num = int(input('Enter the digit what you want to find: '))  # искомое значение X
+for j in array:
+    if j == find_num:
+        count+=1
+print(f'The number of repeated digit is {count}')
+
+#______________________________________________________________________________________________________________________
