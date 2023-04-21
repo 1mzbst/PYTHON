afc1fe99af3c1cb69019d9845c60dfbe07c61740
Dmitriy Kulakov <dk4git@outlook.com>
2023-04-21 18:39:53 +0300

    Task_18 is done

--- PYTHON_COURSE/Home_Work/Task_18.py
+++ PYTHON_COURSE/Home_Work/Task_18.py
@@ -12,14 +12,32 @@
 
 #Решение
 
+from random import randint
 array = []
-count = 0
 numN = int(input('Enter the length of array(N): '))
-print('Enter the digits of the list: ')
-for i in range(1,numN+1):
-    i = int(input())
+# print('Enter the digits of the list: ')
+for i in range(numN):
+    i = randint(0,9)
+    # i = int(input())
     array.append(i)
-print(array)
+print(f'It is the list that we got : {array}')
 find_num = int(input('Enter the digit what you want to find: '))  # искомое значение X
+find_num_for_print = find_num #значение для печати в консоль
+
+# value_check = array[0]
+# value_find = 0
+# for j in array:
+#     value_find = abs(j - find_num) # подсмотерел в интернетах
+#     if value_find < value_check:
+#         result = j
+#         value_check = value_find
+# print(f"The nearest number to {find_num_for_print} is {result} ")
+
+# 
+
+# print(f'Result is {min(array, key = lambda y: abs (y-find_num))}') # подсмотрел в записи с семинара. Крутая штука! :)
+
+
+
 
 #___________________________________________________________________________________________
