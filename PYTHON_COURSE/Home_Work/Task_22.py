4a38bf08799b799626647bd60762dfa1d6190dc7
Dmitriy Kulakov <dk4git@outlook.com>
2023-04-24 13:27:06 +0300

    Task_22 up and done

--- PYTHON_COURSE/Home_Work/Task_22.py
+++ PYTHON_COURSE/Home_Work/Task_22.py
@@ -12,8 +12,6 @@ from random import randint
 first_n = int(input('Enter the count of first set of numbers: '))
 second_n = int(input('Enter the count of second set of numbers: '))
 
-first_set = set()
-second_set = set()
 
 # for i in range(first_n):
 #     i = int(input('Number of the first set, please: '))
@@ -25,19 +23,41 @@ second_set = set()
 #     second_set.add(i)
 # print(second_set)
 
+first_list = []
+second_list = []
+
+# for i in range(first_n):
+#     i = randint(1,15)
+#     first_set.add(i)
+# print(first_set)
+
+# for i in range(second_n):
+#     i = randint(1,15)
+#     second_set.add(i)
+# print(second_set)
+
+print('Lists')
 for i in range(first_n):
     i = randint(1,15)
-    first_set.add(i)
-print(first_set)
+    first_list.append(i)
+print(first_list)
 
 for i in range(second_n):
     i = randint(1,15)
-    second_set.add(i)
+    second_list.append(i)
+print(second_list)
+
+print('Sets')
+first_set = set(first_list) 
+print(first_set)
+second_set = set(second_list)
 print(second_set)
 
 union_sets = first_set.intersection(second_set)
 if union_sets == set(): print('The cross set is empty')
 else: print(f'The cross set is {union_sets}')
+
+
 
 
 
