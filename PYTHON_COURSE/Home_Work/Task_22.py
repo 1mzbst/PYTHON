5a9e55525cc794b96f8bbc50f82d11678d8dcd5c
Dmitriy Kulakov <dk4git@outlook.com>
2023-04-24 13:11:39 +0300

    Task_22 up and done

--- PYTHON_COURSE/Home_Work/Task_22.py
+++ PYTHON_COURSE/Home_Work/Task_22.py
@@ -7,24 +7,37 @@
 
 # Дописать через рандом и положить в списки
 
+from random import randint
+
 first_n = int(input('Enter the count of first set of numbers: '))
 second_n = int(input('Enter the count of second set of numbers: '))
 
 first_set = set()
 second_set = set()
 
+# for i in range(first_n):
+#     i = int(input('Number of the first set, please: '))
+#     first_set.add(i)
+# print(first_set)
+
+# for i in range(second_n):
+#     i = int(input('Number of the second set, please: '))
+#     second_set.add(i)
+# print(second_set)
+
 for i in range(first_n):
-    i = int(input('Number of the first set, please: '))
+    i = randint(1,15)
     first_set.add(i)
 print(first_set)
 
 for i in range(second_n):
-    i = int(input('Number of the second set, please: '))
+    i = randint(1,15)
     second_set.add(i)
 print(second_set)
 
 union_sets = first_set.intersection(second_set)
-print(f'The cross set is {union_sets}')
+if union_sets == set(): print('The cross set is empty')
+else: print(f'The cross set is {union_sets}')
 
 
 
